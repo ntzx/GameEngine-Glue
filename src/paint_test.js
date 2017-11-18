eec6e7b219903ba945756bafe0eafd9d48147b7d
losfair <zhy20000919@hotmail.com>
2017-11-18 23:06:04 +0800

    Accept user code in paint_test

--- src/paint_test.js
+++ src/paint_test.js
@@ -7,7 +7,30 @@ game.load_config(tc);
 
 let board = null;
 
-window.addEventListener("load", () => {
-    board = new paint.Board(game, document.getElementById("game-board"));
+window.build_board_from_code = function() {
+    let code_input = document.getElementById("code-input");
+    let code;
+    try {
+        code = JSON.parse(code_input.value);
+    } catch(e) {
+        alert("JSON 描述文件解析失败。");
+        return;
+    }
+    try {
+        game.load_config(code);
+    } catch(e) {
+        alert("执行系统拒绝了你提供的描述文件，错误信息可在控制台查看。请刷新页面再试。");
+        code_input.disabled = true;
+        document.getElementById("build-board-btn").disabled = true;
+        return;
+    }
+
+    code_input.disabled = true;
+    code_input.style.display = "none";
+
+    let game_board = document.getElementById("game-board");
+    game_board.style.display = "block";
+
+    board = new paint.Board(game, game_board);
     board.draw();
-});
+}
